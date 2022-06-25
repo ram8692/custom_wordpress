ce34b40c7b5f4431c38c79f9a92114cf6c040d75
rambabu.g <rambabu.g@whatarage.com>
2022-06-25 15:25:18 +0530

    content add side menu and submenu and dynamic main menu

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -17,25 +17,44 @@ while (have_posts()) {
 
         <?php
         $theParent = wp_get_post_parent_id(get_the_ID());
-        if($theParent){?>
-        
+        if ($theParent) { ?>
+
             <div class="metabox metabox--position-up metabox--with-home-link">
                 <p>
-                    <a class="metabox__blog-home-link" href="<?=get_permalink($theParent)?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?=get_the_title($theParent)?></a> <span class="metabox__main"><?=get_the_title()?></span>
+                    <a class="metabox__blog-home-link" href="<?= get_permalink($theParent) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?= get_the_title($theParent) ?></a> <span class="metabox__main"><?= get_the_title() ?></span>
                 </p>
             </div>
         <?php } ?>
 
-        <!-- <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
-        <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
-        </ul>
-      </div> -->
+        <!-- this below code does if the current page is not a parent page or child page then dont show the side menu -->
+        <?php
+        //get the list of child pages "get_pages" ,if didnt found then it will return false or null 
+        $testArray = get_pages(array('child_of' => get_the_ID()));
+        if ($theParent or $testArray) {
+        ?>
+            <div class="page-links">
+                <h2 class="page-links__title"><a href="<?= get_the_permalink($theParent) ?>">
+                <!-- get the title of page get_the_title() -->
+                <?= get_the_title($theParent) ?></a></h2>
+                <ul class="min-list">
+                    <?php
+                    if ($theParent) {
+                        $findChildrenOf = $theParent;
+                    } else {
+                        //get_the_ID() return the current page id
+                        $findChildrenOf = get_the_ID();
+                    }
+                    // wp_list_pages() Retrieves or displays a list of pages (or hierarchical post type items) in list (li) format.
+                    wp_list_pages(array('title_li' => NULL, 'child_of' => $findChildrenOf));
+                    ?>
+                </ul>
+            </div>
+        <?php } ?>
 
         <div class="generic-content">
-            <?php the_content(); ?>
+            <?php
+            //page or post content
+            the_content(); ?>
         </div>
     </div>
 
