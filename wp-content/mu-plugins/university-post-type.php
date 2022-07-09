320b87bf0550e93bd0a3e24334bd99509476d8e8
rambabu.g <rambabu.g@whatarage.com>
2022-07-09 22:36:57 +0530

    added cutom fields by acf plugin sorted

--- wp-content/mu-plugins/university-post-type.php
+++ wp-content/mu-plugins/university-post-type.php
@@ -4,8 +4,8 @@
 
 function university_post_types(){
     register_post_type('event',array(
-        'show_in_rest'=>true,
-        'supports'=>array('title','editor','excerpt'),
+        // 'show_in_rest'=>true,
+        'supports'=>array('title','editor','excerpt','custom-fields'),
         'rewrite'=>array('slug'=>'events'),
         'has_archive'=>true,
         'public'=>true,
--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -16,13 +16,18 @@
             <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
             <?php
             //$homePagePosts = new WP_Query(array('posts_per_page' => 2,'category_name'=>'awards','post_type'=>'page'));eg post or page
-            $homePageEvents = new WP_Query(array('posts_per_page' => 2,'post_type'=>'event'));
+            $homePageEvents = new WP_Query(array('posts_per_page' => 2,'post_type'=>'event','orderby'=>'post_date','order'=>'ASC'));
             while ($homePageEvents->have_posts()) {
                 $homePageEvents->the_post(); ?>
             <div class="event-summary">
                 <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month"><?php the_time('M') ?></span>
-                    <span class="event-summary__day"><?php the_time('d') ?></span>
+                    <span class="event-summary__month"><?php //the_time('M')
+                    //the_field('event_date');
+                    $eventDate = new DateTime(get_field('event_date'));
+                    echo $eventDate->format('M');
+                    ?></span>
+                    <span class="event-summary__day"><?php //the_time('d')
+                    echo $eventDate->format('d') ?></span>
                 </a>
                 <div class="event-summary__content">
                     <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h5>
