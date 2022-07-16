8c43767a9805b0e47caa3b7aa21e868d8f90303f
rambabu.g <rambabu.g@whatarage.com>
2022-07-16 22:48:57 +0530

    cutom query pagination (past events)

--- wp-content/themes/fictional-university-theme/page-past-events.php
+++ wp-content/themes/fictional-university-theme/page-past-events.php
@@ -3,17 +3,36 @@
     <div class="page-banner__bg-image" style="background-image: url(<?= get_theme_file_uri('images/ocean.jpg') ?>)"></div>
     <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title">
-            All Events
+            Past Events
         </h1>
         <div class="page-banner__intro">
-            <p>see what is going on</p>
+            <p>a recap of all our past events</p>
         </div>
     </div>
 </div>
 <div class="container container--narrow page-section">
     <?php
-    while (have_posts()) {
-        the_post(); ?>
+
+    $today = date('Ymd');
+    $pastEvents = new WP_Query(array(
+        'paged'=>get_query_var('paged',1),
+        'posts_per_page' => 1,
+        'post_type' => 'event',
+        'meta_key' => 'event_date',
+        'orderby' => 'meta_value_num',
+        'order' => 'ASC',
+        'meta_query' => array(
+            array(
+                'key' => 'event_date',
+                'compare' => '<',
+                'value' => $today,
+                'type' => 'numeric'
+            )
+        )
+    ));
+
+    while ($pastEvents->have_posts()) {
+        $pastEvents->the_post(); ?>
         <div class="event-summary">
             <a class="event-summary__date t-center" href="#">
                 <span class="event-summary__month"><?php //the_time('M')
@@ -32,10 +51,8 @@
 
     <?php }
 
-    echo paginate_links();
+    echo paginate_links(array('total'=>$pastEvents->max_num_pages));
     ?>
-<hr class="section-break">
-<p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events')?>">check out our past events archive</a></p>
 
 </div>
 
